9d0146b33c4e1de6cea5424149374fba701b88ca
64160010 <64160010@go.buu.ac.th>
2024-07-28 22:54:26 +0700

    AdminOkay

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\VerificationController;
+use App\Http\Middleware\IsAdmin;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+
+Auth::routes();
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+// เส้นทางสำหรับหน้าแรกของผู้ดูแลระบบ
+Route::get('/admin/home', [HomeController::class, 'adminHome'])
+    ->name('admin.home')
+    ->middleware(IsAdmin::class);
